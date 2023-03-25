69be1b6c6d812abc0b49b4b2e02b6c8fb28d24aa
Владислав Жуковский <jaymzmanunited@gmail.com>
2023-03-25 21:53:07 +0300

    updates

--- 06-functions-passing-params/main.cpp
+++ 06-functions-passing-params/main.cpp
@@ -14,18 +14,49 @@ void pass_by_pointer(int* a)
     (*a)++;
 }
 
+void pass_by_const_pointer(int* const a)
+{
+    (*a)++;
+}
+
 void pass_by_reference(int& a)
 {
     ++a;
 }
 
-int main()
+void m01()
 {
     int a = 1;
+
+    cout << a;
+    cout << &a;
+
     pass_by_value(a);
     cout << a;
     pass_by_pointer(&a);
     cout << a;
+    pass_by_const_pointer(&a);
+    cout << a;
     pass_by_reference(a);
     cout << a;
+}
+
+void m02()
+{
+    int b = 1;
+    int& a = b;
+
+    cout << a;
+    cout << &a;
+    cout << "\n";
+
+    pass_by_value(a);
+    cout << a;
+    pass_by_reference(a);
+    cout << a;
+}
+
+int main()
+{
+    m02();
 }
